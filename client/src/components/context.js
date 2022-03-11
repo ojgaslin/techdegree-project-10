4424eaa4f39bbda4c854e7ecc7f467b286fa1d91
ogaslin <olivia.gaslin@gmail.com>
2022-03-10 22:49:45 -0500

    Update context.js in client
    made functions in context for children components to be able to update context

--- client/src/components/context.js
+++ client/src/components/context.js
@@ -4,4 +4,15 @@ export const userContext = React.createContext({user: {
     firstName: '',
     lastName: '',
     password: ''
-}});
+},
+signIn: (user) =>{
+    firstName = user.firstName,
+    lastName = user.lastName,
+    password = user.password
+},
+signOut: () => {
+    firstName = "",
+    lastName = "",
+    password = ""
+}
+});
